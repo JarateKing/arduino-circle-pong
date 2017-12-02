392d7647f9b204aff4a7a32801d6fa06e2b99f97
JarateKing <scmacintosh@upei.ca>
2017-12-02 13:49:47 -0400

    added paddle position arrays

--- src/display/display.ino
+++ src/display/display.ino
@@ -1,3 +1,13 @@
+// inputting the position, these arrays will give you what column & row to enable
+// hori is for the top and bottom display cycle, verti is for the left and right sides
+// position goes clockwise from the topright corner, 0-27
+// -1 is for not enabling anything
+int horiColumns[28] = {0,1,2,3,4,5,6,7,-1,-1,-1,-1,-1,-1,7,6,5,4,3,2,1,0,-1,-1,-1,-1,-1,-1};
+int horiRows[28] = {0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,7,7,7,7,7,7,7,7,-1,-1,-1,-1,-1,-1};
+
+int vertiColumns[28] = {0,-1,-1,-1,-1,-1,-1,7,7,7,7,7,7,7,7,-1,-1,-1,-1,-1,-1,0,0,0,0,0,0,0};
+int vertiRows[28] = {0,-1,-1,-1,-1,-1,-1,0,1,2,3,4,5,6,7,-1,-1,-1,-1,-1,-1,7,6,5,4,3,2,1};
+
 void setup() {
   // put your setup code here, to run once:
 
